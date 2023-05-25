e871489382f33c87aa55ae4ad55cd26c2eea50da
贺斌 <mail_joker@163.com>
2023-05-25 12:24:51 +0800

    启动配置方式修改

--- app/src/main/java/com/example/appmoudle/MainApplication.kt
+++ app/src/main/java/com/example/appmoudle/MainApplication.kt
@@ -1,6 +1,13 @@
 package com.example.appmoudle
 
+import android.util.Log
 import androidx.multidex.MultiDexApplication
+import com.example.appmoudle.startup.CommonStartup
+import com.rousetime.android_startup.StartupListener
+import com.rousetime.android_startup.StartupManager
+import com.rousetime.android_startup.model.CostTimesModel
+import com.rousetime.android_startup.model.LoggerLevel
+import com.rousetime.android_startup.model.StartupConfig
 
 /**
  * CreateBy:Joker
@@ -11,8 +18,31 @@ class MainApplication : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
+        initStartConfig()
+    }
 
+    /**
+     * 初始化启动配置
+     */
+    private fun initStartConfig() {
+        val config = StartupConfig.Builder()
+            .setLoggerLevel(LoggerLevel.DEBUG)       // default LoggerLevel.NONE
+            .setAwaitTimeout(12000L)                 // default 10000L
+            .setOpenStatistics(true)                 // default true
+            .setListener(object : StartupListener {
+                override fun onCompleted(totalMainThreadCostTime: Long, costTimesModels: List<CostTimesModel>) {
+                    // can to do cost time statistics.
+                    Log.i("StartupTrack", "onCompleted:totalMainThreadCostTime=$totalMainThreadCostTime size=${costTimesModels.size}")
+                }
+            })
+            .build()
 
+        StartupManager.Builder()
+            .setConfig(config)
+            .addStartup(CommonStartup())
+            .build(this)
+            .start()
+            .await()
     }
 
 
